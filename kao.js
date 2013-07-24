9d08324c434fcc783b4b36550c49498c3de6ad41
xwcoder <xwcoder@outlook.com>
2013-07-24 16:43:11 +0800

    增加debug模式
    * 开启debug模式1:kao.debug = true;
    * 开启debug模式2: <script src="kao.js" data-debug="true"></script>
    * debug模式会删除掉加载的script节点
    * debug模式提供若干配置项, { debugConfig }
        * convert: url转换为对src文件的请求, 默认值为'all',  转换函数为converFn
            - 'all' : 所有url转换为对src<源文件>的请求
            - RegExp :  所有匹配此正则的url进行转换
            - function :  自定义函数，参数为原始url，返回值为true进行转换，
                        false不转换
            - false : 不进行转换
        * convertFn : 转换函数
        * convertExclude: array，直接跳过不转换的url，数组项可以是以下值:
            - string : 直接与url做相等匹配
            - RegExp : url配置此正则则不转换
            - function : 自定义函数, 参数为原始url，返回值为true不进行转换,
                        false进行转换
        convertExclude优先级高于convert
    * kao.setDebugConfig: 对debug模式的参数进行设置

--- kao.js
+++ kao.js
@@ -1,4 +1,4 @@
-(function (window, document) {
+( function ( window, document ) {
     var mb = new MessageBus();
     var slice = Array.prototype.slice;
     var toString = Object.prototype.toString;
@@ -9,17 +9,17 @@
         return 'kao-anony-mod' + ++gid;
     };
     
-    var throwException = function(msg){
-        throw new Error(msg);
+    var throwException = function ( msg ) {
+        throw new Error( msg );
     };
 
     var script = function () {
-        var scripts = document.getElementsByTagName('script');
+        var scripts = document.getElementsByTagName( 'script' );
         var i = 0, len = scripts.length, script;
         var reg = /kao\.js(?=[\?#]|$)/;
-        for (; i < len; i++) {
-            script = scripts[i];
-            if (reg.exec(script.getAttribute('src') || '')) {
+        for ( ; i < len; i++ ) {
+            script = scripts[ i ];
+            if ( reg.exec( script.getAttribute( 'src' ) || '' ) ) {
                 break;
             }
             script = null;
@@ -27,45 +27,85 @@
         return script;
     }();
 
-    var isScript = function (filename) {
+    var isScript = function ( filename ) {
         filename = filename || '';
-        return !!/\.js(?=[\?#]|$)/i.exec(filename);
+        return !!/\.js(?=[\?#]|$)/i.exec( filename );
     };
 
-    var isCss = function (filename) {
+    var isCss = function ( filename ) {
         filename = filename || '';
-        return !!/\.css(?=[\?#]|$)/i.exec(filename);
+        return !!/\.css(?=[\?#]|$)/i.exec( filename );
     };
 
-    var isFunction = function (fn) {
-        return fn && toString.call(fn) === '[object Function]';
+    var isFunction = function ( fn ) {
+        return fn && toString.call( fn ) === '[object Function]';
     };
 
-    var isString = function (s) {
-        return s && toString.call(s) === '[object String]';
+    var isString = function ( s ) {
+        return s && toString.call( s ) === '[object String]';
     };
 
-    var isArray = function (a) {
-        return a && toString.call(a) === '[object Array]';
+    var isArray = function ( a ) {
+        return a && toString.call( a ) === '[object Array]';
     };
 
-    var isObject = function (o) {
-        return o && toString.call(o) === '[object Object]';
+    var isObject = function ( o ) {
+        return o && toString.call( o ) === '[object Object]';
     };
-    
 
-    var apply = function (o, c) {
-        if (o && c) {
-            for (var p in c) {
-                o[p] = c[p];
+    var isRegExp = function ( o ) {
+        return o && toString.call( o ) === '[object RegExp]';
+    };
+
+
+    var apply = function ( o, c ) {
+        if ( o && c ) {
+            for ( var p in c ) {
+                o[ p ] = c[ p ];
             }
         }
     };
 
     var config = {
-        baseURL : 'http://tv.sohu.com/upload/',
+        baseURL : 'http://js.tv.itc.cn/',
 
-        coreLib : 'http://tv.sohu.com/upload/jq_plugin/j.js'
+        coreLib : 'http://js.tv.itc.cn/base/core/g13071501.js'
+    };
+
+    var debugConfig = {
+        convert : 'all',
+        convertFn : function ( url ) {
+
+            var exclude = this.convertExclude;
+
+            for ( var i = 0, len = exclude.length; i < len; i++ ) {
+                if ( isString( exclude[ i ] ) && url == exclude[ i ]
+                || isRegExp( exclude[ i ] ) && exclude[ i ].test( url)
+                || isFunction( exclude[ i ] ) && exclude[ i ]( url ) ) {
+                    return url;
+                }
+            }
+
+            if ( /\.src\./.test( url ) ) {
+                return url;
+            }
+            var reg = /\.(\d+)\.js(#|\?|\.|$)/; //core/g.13071501.js
+            var m;
+            if ( m = reg.exec( url ) ) {
+                return url.replace( m[ 1 ], 'src' );
+            }
+            
+            reg = /\w+?(\d+)\.js(#|\?|\.|$)/; //core/g13071501.js
+            
+            if ( m = reg.exec( url ) ) {
+                return url.replace( m[ 1 ], '.src' );               
+            }
+
+            return url;
+        },
+        convertExclude : [
+            'http://js.tv.itc.cn/base/core/j_1.7.2.js'
+        ]
     };
 
     var loaded = {};
@@ -74,141 +114,154 @@
 
     var modules = {};
     
-    var getModule = function (name) {
+    var getModule = function ( name ) {
         var mod;
-        if (isString(name)) {
-            mod = modules[name];
+        if ( isString( name ) ) {
+            mod = modules[ name ];
         }
         return mod;
     };
 
-    var getUrl = function (path) {
+    var getUrl = function ( path ) {
         path = path || '';
-        if (!/^http(s)?:\/\//.exec(path)) {
+        if ( !/^http(s)?:\/\//.exec( path ) ) {
             path = config.baseURL + path;
+        }
+        if ( kao.debug && debugConfig.convert ) {
+            if ( debugConfig.convert == 'all'
+                || isRegExp( debugConfig.convert ) && debugConfig.convert.test( path )
+                || isFunction( debugConfig.convert ) && debugConfig.convert( path ) ) {
+
+                path = debugConfig.convertFn( path );
+            }
         }
         return path;
     };
 
-    var loadCss = function (url, charset) {
-        var node = document.createElement('link');
-        node.setAttribute('type', 'text/css');
-        node.setAttribute('rel', 'stylesheet');
-        node.setAttribute('href', url);
+    var loadCss = function ( url, charset ) {
+        var node = document.createElement( 'link' );
+        node.setAttribute( 'type', 'text/css' );
+        node.setAttribute( 'rel', 'stylesheet' );
+        node.setAttribute( 'href', url );
 
-        if (charset) {
+        if ( charset ) {
             node.charset = charset;
         }
         
-        script.parentNode.insertBefore(node, script);
+        script.parentNode.insertBefore( node, script );
     };
 
-    var load = function (modName, url, type, charset, fn) {
-        url = getUrl(url);
-        var topic = url.replace(/[\/:]/g, '');
+    var load = function ( modName, url, type, charset, fn ) {
+        url = getUrl( url );
+        var topic = url.replace( /[\/:]/g, '' );
         var node, sid;
 
         var iscss = type === 'css' || isCss(url);
 
-        if (iscss) {
-            loadCss(url, charset);
+        if ( iscss ) {
+            loadCss( url, charset );
             return;
         }
 
-        if (loaded[url]) {
+        if ( loaded[ url ] ) {
             fn && fn();
-            modName && mb.publish(modName);
+            modName && mb.publish( modName );
             return;
         } else {
-            sid = mb.subscribe(topic, function () {
+            sid = mb.subscribe( topic, function () {
                 fn && fn();
-                modName && mb.publish(modName);
-            }, this, null, {execTime : 1});
+                modName && mb.publish( modName );
+            }, this, null, { execTime : 1 } );
         }
 
-        if (loading[url]) {
+        if ( loading[ url ] ) {
             return;
         }
         
-        loading[url] = 1;
+        loading[ url ] = 1;
 
-        node = document.createElement('script');
-        node.setAttribute('type', 'text/javascript');
-        node.setAttribute('src', url);
-        node.setAttribute('async', true);
-        if (charset) {
+        node = document.createElement( 'script' );
+        node.setAttribute( 'type', 'text/javascript' );
+        node.setAttribute( 'src', url );
+        node.setAttribute( 'async', true );
+        if ( charset ) {
           node.charset = charset;
         }
 
         node.onerror = function() {
             mb.unsubscribe(sid);
-            loading[url] = 0;
+            loading[ url ] = 0;
             node.onerror = null;
         };
-        var done = false
-        node.onload = node.onreadystatechange = function() {
-            if (!done && (!this.readyState || this.readyState === 'loaded' 
-                    || this.readyState === 'complete')) {
+        var done = false;
+        node.onload = node.onreadystatechange = function () {
+            if ( !done && ( !this.readyState || this.readyState === 'loaded' 
+                    || this.readyState === 'complete' ) ) {
                 done = true;
                 node.onload = node.onreadystatechange = null;
 
-                loaded[url] = 1;
-                loading[url] = 0;
+                loaded[ url ] = 1;
+                loading[ url ] = 0;
+
+                if ( !kao.debug ) {
+                    script.parentNode.removeChild( node );
+                }
                 mb.publish(topic);
             }
         };
 
-        script.parentNode.insertBefore(node, script);
+        script.parentNode.insertBefore( node, script );
     };
 
-    var loadOrder = function (deps, fn) {
-        var modNames = createModules(deps);
+    var loadOrder = function ( deps, fn ) {
+        var modNames = createModules( deps );
 
         var modName, mod, i = 0;
         var sid;
 
-        sid = mb.wait(modNames, function () {
-            mb.unsubscribe(sid);
+        sid = mb.wait( modNames, function () {
+            mb.unsubscribe( sid );
             fn && fn();
-        }, this);
+        }, this );
 
-        while(modName = modNames[i++]){
-            mod = getModule(modName);
-            if (mod.requires && mod.requires.length) {
-                loadOrder(mod.requires, (function (modName, mod) {
-                    return function (){
-                        load(modName, mod.path, mod.type, mod.charset);
+        while ( modName = modNames[ i++ ] ) {
+            mod = getModule( modName );
+            if ( mod.requires && mod.requires.length ) {
+                loadOrder( mod.requires, ( function ( modName, mod ) {
+                    return function () {
+                        load( modName, mod.path, mod.type, mod.charset );
                     }   
-                })(modName, mod));
+                } )( modName, mod ) );
             } else {
-                load(modName, mod.path, mod.type, mod.charset);
+                load( modName, mod.path, mod.type, mod.charset );
             }
         };
     };
 
-    var createModules = function (mods, names) {
+    var createModules = function ( mods, names ) {
         names = names || [];
-        var mod, i = 0;
-        while (mod = mods[i++]) {
+        var mod, name, i = 0;
 
-            if (isString(mod)) {
+        while ( mod = mods[ i++ ] ) {
 
-                if (modules[mod]) {
+            if ( isString( mod ) ) {
 
-                    names.push(mod);
+                if ( modules[ mod ] ) {
 
-                } else if (isScript(mod) || isCss(mod)) {
+                    names.push( mod );
+
+                } else if ( isScript( mod ) || isCss( mod ) ) {
 
                     name = getAnonyModuleName();
-                    kao.add(name, {path : mod});
-                    names.push(name);
+                    kao.add( name, { path : mod } );
+                    names.push( name );
 
                 }
-            } else if (isObject(mod) && mod.path){
+            } else if ( isObject( mod ) && mod.path ) {
 
                 name = getAnonyModuleName();
-                kao.add(name, mod);
-                names.push(name);
+                kao.add( name, mod );
+                names.push( name );
             } 
         }
         return names;
@@ -224,68 +277,77 @@
     //kao(false, 'mod1', 'js/g.js', 'mod2', {path : 'js/gNav.js', requires : []});
     //kao('mod3', function(){});
     var kao = function () {
-        if ( !modules['kao-corelib'] ) {
-            modules['kao-corelib'] = { path : config.coreLib }
+        
+        if ( !modules[ 'kao-corelib' ] ) {
+            modules[ 'kao-corelib' ] = { path : config.coreLib }
         }
-        var args = slice.call(arguments); 
+
+        var args = slice.call( arguments ); 
         var fn;
         var requireCore = true;
 
-        if (args[0] === false) {
+        if ( args[ 0 ] === false ) {
             requireCore = args.shift();
         }
         
-        if (isFunction(args[args.length - 1])) {
+        if ( isFunction( args[ args.length - 1 ] ) ) {
             fn = args.pop();
         }
 
-        if (requireCore) {
-            load('kao-corelib', getModule('kao-corelib').path, null, null, (function (args, fn) {
+        if ( requireCore ) {
+            load( 'kao-corelib', getModule( 'kao-corelib' ).path, null, null, ( function ( args, fn ) {
                 return function () {
-                    if (!args || !args.length) {
+                    if ( !args || !args.length ) {
                         fn && fn();
                     } else {
-                        loadOrder(args, fn);
+                        loadOrder( args, fn );
                     }
                 }
-            })(args, fn));
+            } ) ( args, fn ) );
         } else {
-            if (!args || !args.length) {
+            if ( !args || !args.length ) {
                 fn && fn();
             } else {
-                loadOrder(args, fn);
+                loadOrder( args, fn );
             }
         }
     };
 
-    apply(kao, {
+    apply( kao, {
         //add(name, 'js/jNav.js');
         //add(name, {path:'js/g.js', requires:[], charset : '', type : js});
-        add : function (modName, config) {
+        add : function ( modName, config ) {
             var mod;
-            if (!isString(modName)) {
+            if ( !isString( modName ) ) {
                 return;
             }
 
-            if (isString(config)) {
-                mod = {path : config};
-            } else if (isObject(config) && config.path) {
+            if ( isString( config ) ) {
+                mod = { path : config };
+            } else if ( isObject( config ) && config.path ) {
                 mod = config;
             }
 
-            if (mod) {
-                modules[modName] = mod;
+            if ( mod ) {
+                modules[ modName ] = mod;
             }
         },
 
-        setConfig : function (o) {
-            apply(config, o);
+        setConfig : function ( o ) {
+            apply( config, o );
+        },
+
+        debug : false,
+
+        setDebugConfig : function ( o ) {
+            apply( debugConfig, o );
         }
-    });
+
+    } );
 
     window.kao = kao;
 
-    if (ext = script.getAttribute('data-corelib')) {
+    if ( ext = script.getAttribute('data-corelib') ) {
         config.coreLib = ext;
     }
 
@@ -293,11 +355,16 @@
         config.baseURL = ext;
     }
 
-    if (ext = script.getAttribute('data-main')) {
-        if (script.getAttribute('data-main-needcore') === 'false') {
-            kao(false, ext);
+    if ( ( ext = script.getAttribute( 'data-debug' ) ) == 'true' ) {
+        kao.debug = true;
+    }
+
+    if ( ext = script.getAttribute( 'data-main' ) ) {
+        if ( script.getAttribute('data-main-needcore' ) === 'false' ) {
+            kao( false, ext );
         } else {
-            kao(ext);
+            kao( ext );
         }
     }
-})(window, document);
+} )( window, document );
+kao.debug = true;
